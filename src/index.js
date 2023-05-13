67b4dc9ce453add9928658454b84d5f73ba97c01
skenrobert <skenrobert@gmail.com>
2023-05-13 13:49:03 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {Greeting, CreateObj} from './example'; // export N component, that have many component
+import Product from "./product"; './product'; //export whole component, that have only one component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// function Greeting() {//all component need create with function syntax the first character in Uppercase
+//     const name = "Kenny";
+//     const married = false;
+  
+//     return <h1>{married ? "I'm married " + married.toString() : "I'm don't married " + married.toString()}</h1>;
+  
+//     if (married) {
+//       return <h1>I'm married</h1>;
+//     }
+  
+//     return <h1> Hello World, {name}</h1>;
+//   }
+  
+//   function CreateObj() {//all component need create with function syntax the first character in Uppercase
+//       const user = {
+//           firstName: 'Kenny',
+//           lastname: 'Mora'
+//       }
+  
+//       // return <h1> {JSON.stringify(user)} </h1>
+  
+//       function age(birthYear){ // function in component
+//           let today = new Date();
+//           let year = today.getFullYear();
+//           return year - birthYear
+//       }
+  
+//       return(
+//           <div>
+//               <h1> {user.firstName} </h1>
+//               <h1> {user.lastname} </h1>
+//               <h1> { age(1900) } </h1>
+//           </div>
+//       );
+//   }
+
+//send components to index.html (div id=root)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <>
+    {/* { Greeting() } */}
+    {/* is the same this */}
+    {/* <Greeting></Greeting> */}
+    {/* and then */}
+    <Greeting />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <CreateObj />
+  </>
+);
---
+++ src/product.js
@@ -0,0 +1,10 @@
+function Product() {
+  return (
+    <div>
+      <h1> Product</h1>
+    </div>
+  );
+}
+
+// other form export component
+export default Product
